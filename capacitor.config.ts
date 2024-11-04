301c65072c5ffc2d2d28da39f6b2fd48ab07a16e
David Vela <david.vela@epn.edu.ec>
2024-11-03 20:29:35 -0500

    feat(UI): Add splash screen and icon

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,27 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
-const config: CapacitorConfig = {
+const config: CapacitorConfig={
   appId: 'io.ionic.starter',
   appName: 'primera_app_ionic_24b',
-  webDir: 'www'
+  webDir: 'www',
+  "plugins": {
+    "SplashScreen": {
+      "launchShowDuration": 0,
+      "launchAutoHide": true,
+      "launchFadeOutDuration": 3000,
+      "backgroundColor": "#ffffffff",
+      "androidSplashResourceName": "splash",
+      "androidScaleType": "CENTER_CROP",
+      "showSpinner": false,
+      "androidSpinnerStyle": "large",
+      "iosSpinnerStyle": "small",
+      "spinnerColor": "#999999",
+      "splashFullScreen": false,
+      "splashImmersive": false,
+      "layoutName": "launch_screen",
+      "useDialog": false
+    }
+  }
 };
 
 export default config;
